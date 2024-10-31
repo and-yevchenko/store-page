801641bbc17e383b96c495a9f22ec5144e2ef2d1
Andrii Yevchenko <and.yevchenko@gmail.com>
2024-10-31 18:42:31 +0200

    Add logo

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import { Logo } from '../Logo/Logo'
+import './Header.css'
+
+export const Header = () => {
+
+    return (
+        <header className='header'>
+            <Logo />
+        </header>
+    )
+}
---
+++ src/components/Logo/Logo.jsx
@@ -0,0 +1,8 @@
+import './Logo.css'
+
+export const Logo = () => {
+
+    return (
+        <img className='logo' src="logo.svg" alt="logo" />
+    )
+}
