4be1a232ee3c5c0c1c38ccfa536cbd69d330a1d9
Andrii Yevchenko <and.yevchenko@gmail.com>
2024-11-02 20:41:09 +0200

    Add ProductTabs

--- src/components/Description/Description.jsx
+++ src/components/Description/Description.jsx
@@ -4,6 +4,7 @@ import { Heart, ShoppingCart } from 'lucide-react'
 import { ProductName } from '../Product/ProductName'
 import { ProductSize } from '../Product/ProductSize'
 import { ProductChoose } from '../Product/ProductChoose'
+import { ProductTabs } from '../Product/ProductTabs'
 
 export const Description = ({data}) => {
 
@@ -12,34 +13,10 @@ export const Description = ({data}) => {
             <ProductName data={data}/>
             <ProductSize data={data}/>
             <ProductChoose data={data}/>
+            <ProductTabs data={data}/>
            
 
-            <Box>
-                <Tabs.Root defaultValue='suggestions'>
-                    <Tabs.List color='gray'>
-                        <Tabs.Trigger value='descripton' style={{color: 'var(--color-main)'}}>Description</Tabs.Trigger>
-                        <Tabs.Trigger value='suggestions' style={{color: 'var(--color-main)'}}>Suggestions</Tabs.Trigger>
-                        <Tabs.Trigger value='shipment' style={{color: 'var(--color-main)'}}>Shipment</Tabs.Trigger>
-                    </Tabs.List>
-                    <ScrollArea type="auto" scrollbars="vertical" style={{ height: 120 }}>
-                        <Box pt="3">
-                            <Tabs.Content value="descripton">
-                                <Text as='p' size="2" style={{color: 'var(--color-main)'}}>- Nike Dri-FIT technology</Text>
-                                <Text as='p' size="2" style={{color: 'var(--color-main)'}}>- High-stretch, quick-drying fabric</Text>
-                                <Text as='p' size="2" style={{color: 'var(--color-main)'}}>- 100% recycled polyester</Text>
-                                <Text as='p' size="2" style={{color: 'var(--color-main)'}}>- Color: Blue and scarlet.</Text>
-                            </Tabs.Content>
-                            <Tabs.Content value="suggestions">
-                                <Text size="2">Access and update your documents.</Text>
-                            </Tabs.Content>
-                            <Tabs.Content value="shipment">
-                                <Text as='p' size="2" style={{color: 'var(--color-main)'}}>The delivery timeframe for personalized products is 7 to 14 days.</Text>
-                                <Text as='p' size="2" style={{color: 'var(--color-main)'}}>This product cannot be exchanged or returned.</Text>
-                            </Tabs.Content>
-                        </Box>
-                    </ScrollArea>
-                </Tabs.Root>
-            </Box>
+            
 
             <Flex gap="1" gapX='9'>
                 <Box>
---
+++ src/components/Product/ProductTabs.jsx
@@ -0,0 +1,29 @@
+import { Box, ScrollArea, Tabs, Text } from "@radix-ui/themes"
+
+export const ProductTabs = ({ data }) => {
+
+    const { info } = data
+
+    return (
+        <Box>
+            <Tabs.Root defaultValue={info[0].title}>
+                <Tabs.List color='gray'>
+                    {info.map((trigger, index) => (
+                        <Tabs.Trigger key={index} value={trigger.title} style={{color: 'var(--color-main)'}}>{trigger.title}</Tabs.Trigger>
+                    ))}
+                </Tabs.List>
+                <ScrollArea type="auto" scrollbars="vertical" style={{ height: 120 }}>
+                    <Box pt="3">
+                        {info.map((content, index) => (
+                            <Tabs.Content key={index} value={content.title}>
+                                {content.text.map((txt, indx) => (
+                                    <Text key={indx} as='p' size="2" style={{color: 'var(--color-main)'}}>{txt}</Text>
+                                ))}
+                            </Tabs.Content>
+                        ))}
+                    </Box>
+                </ScrollArea>
+            </Tabs.Root>
+        </Box>
+    )
+}
